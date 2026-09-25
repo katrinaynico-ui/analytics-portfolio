e7842367fb28857af2f3f386d13e4eb0fd275ec3
katrinaynico-ui <katrinaynico@gmail.com>
2026-09-25 22:22:41 +0200

    Initial portfolio: 4 analytics projects + Streamlit hub

---
+++ projects/03-dbt-duckdb-analytics/macros/generate_schema_name.sql
@@ -0,0 +1,9 @@
+{# Use custom schema as-is (staging / intermediate / marts) instead of main_staging. #}
+{% macro generate_schema_name(custom_schema_name, node) -%}
+    {%- set default_schema = target.schema -%}
+    {%- if custom_schema_name is none -%}
+        {{ default_schema }}
+    {%- else -%}
+        {{ custom_schema_name | trim }}
+    {%- endif -%}
+{%- endmacro %}
